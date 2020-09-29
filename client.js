5ce04de09eac0f0de39a63e01f3cc70f16f8560a
Nazar <nmeredov@evolutiongaming.com>
2020-09-29 12:51:42 +0300

    update the project

--- client.js
+++ client.js
@@ -1,20 +1,98 @@
 const fetch = require('node-fetch');
 
-const sessionId = '2a47ffce1103cf1313a153390f23ad86b71ef89e';
+var setEnvironmentVariable = {
+  getRandomId: function (idLength) {
+    let id = 'inttest';
+    let idPrefixLength = id.length;
+    let charset = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < idLength - idPrefixLength; i++)
+      id += charset.charAt(Math.floor(Math.random() * charset.length));
+    return id;
+  },
+  getRandomNumericId: function (idLength) {
+    let id = '707';
+    let idPrefixLength = id.length;
+    let charset = '123456789';
+    for (let i = 0; i < idLength - idPrefixLength; i++)
+      id += charset.charAt(Math.floor(Math.random() * charset.length));
+    return id;
+  },
+  generateRandomIdAndNumericId: function (idLength) {
+    let idGeneration = {
+      randomId: this.getRandomId(idLength),
+      randomNumericId: this.getRandomNumericId(idLength)
+    };
+    return idGeneration;
+  }
+};
 
-const raw = JSON.stringify ({
-  'correlationId':'00000174c629110700340000062a95c9',
-  'sessionId':sessionId,
-  'balanceId':'combined'
-})
+const correlationId = setEnvironmentVariable.getRandomId(32);
+const sessionId = setEnvironmentVariable.getRandomId(32);
+const licenseeSessionId = setEnvironmentVariable.getRandomId(32);
 
-const requestOptions = {
+// console.log(correlationId);
+// console.log(sessionId);
+// console.log(licenseeSessionId);
+
+
+
+const raw1 = JSON.stringify({
+  correlationId: correlationId,
+  sessionId: sessionId,
+  balanceId: 'combined',
+  casinoId: 'xtreme0000000001',
+  licenseeSessionId: licenseeSessionId,
+  currency: 'EUR',
+  licenseePlayerId: 'vm000000000001',
+  channel: {
+    type: 'Phone',
+    wrapped: false,
+    os: 'Other'
+  },
+  clientIpAddress: '127.0.0.1'
+});
+
+const raw2 = JSON.stringify({
+  correlationId: correlationId,
+  sessionId: sessionId,
+  balanceId: 'combined'
+});
+
+const requestOptions1 = {
+  method: 'PUT',
+  body: raw1,
+  headers: { 'Content-Type': 'application/json' }
+};
+
+const requestOptions2 = {
+  method: 'PUT',
+  body: raw1,
+  headers: { 'Content-Type': 'application/json' }
+};
+
+const requestOptions3 = {
   method: 'POST',
-  body: raw,
-  headers: {'Content-Type' : 'application/json'}
-}
+  body: raw2,
+  headers: { 'Content-Type': 'application/json' }
+};
 
- fetch('http://10.10.88.52:9092/onewallet/api3/get_balance', requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error))
+fetch(
+  'http://10.10.88.52:9092/onewallet/api3/start_session_initialization',
+  requestOptions1
+)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.log('error', error));
+
+fetch(
+  'http://10.10.88.52:9092/onewallet/api3/complete_session_initialization',
+  requestOptions2
+)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.log('error', error));
+
+fetch('http://10.10.88.52:9092/onewallet/api3/get_balance', requestOptions3)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.log('error', error));
